dd6e78d664a682c3e435ef7bea458a6ff36bd343
Ekk Store <ekkstore.id@gmail.com>
2026-09-09 19:44:52 +0700

    Create update-profile.js

---
+++ api/update-profile.js
@@ -0,0 +1,7 @@
+// api/update-profile.js
+export default async function handler(req, res) {
+  return res.status(200).json({ 
+    success: true, 
+    message: "update-profile.js is working!" 
+  });
+}
